9c221c55ebfb8f6265fd71294aeb535a20704bc5
Fevzi Atanoğlu <fevziatan2@gmail.com>
2022-11-21 15:55:06 +0300

    server-deletestudent and addstudent(update)!

--- server/course-router.js
+++ server/course-router.js
@@ -9,54 +9,119 @@ const { validationResult, body } = require("express-validator");
 
 
 
+// ==================== ADD STUDENT TO COURSE ========================================================================================================================
 router.post("/addstudent", async (req, res) => {
-
-
     try {
-        
+
         const studentId = req.body.studentId;
-        const student = await Student.findById({_id : studentId});
 
-        const course = await Course.findOne({name:req.body.courseName});
+        const student = await Student.findById({ _id: studentId });
 
-        course.students.push(student);
-        student.courses.push(req.body.courseName);
+        const course = await Course.findOne({ name: req.body.courseName });
 
-        await student.save();
-        await course.save();
-        
-     
 
-        
 
-       
+        const studentsOfCourse = Array.from(course.students);
 
-        return res.send({ course:course });
+        let isStudentInCourse = studentsOfCourse.filter(findStudent => (findStudent._id !== student._id))
+
+
+
+
+        if (!(isStudentInCourse.length)) {
+
+            student.courses.push(course.name);
+            course.students.push(student);
+
+            await student.save();
+            await course.save();
+
+            return res.send({ message: `${student.name}  ${student.surname} isimli öğrenci ${course.name} kursuna başarıyla eklendi.`, success: true });
+
+        } else {
+
+            return res.send({ message: `${student.name}  ${student.surname} zaten ${course.name} ekli.`, success: false });
+
+        }
+
+
     } catch (e) {
-        return res.send({ e: e, m: "error" });
+        return res.send({ e: e, m: "error", success: false });
     }
 });
 
 
+// ==================== DELETE STUDENT FROM ARRAY =====================================================================================================================
+
+router.post("/deletestudent", async (req, res) => {
+    try {
+
+        const studentId = req.body.studentId;
+
+        const student = await Student.findById({ _id: studentId });
+
+        const course = await Course.findOne({ name: req.body.courseName });
+
+
+
+        const studentsOfCourse = Array.from(course.students);
+
+        let isStudentInCourse = studentsOfCourse.filter(findStudent => (findStudent._id !== student._id))
+
+
+
+
+        if ((isStudentInCourse.length)) {
+
+            // student.courses.push(course.name);
+            console.log(student.courses.indexOf(course.name));
+            let a = student.courses.indexOf(course.name)
+            student.courses.splice(a,1);
+            console.log(student.courses);
+
+            await student.save()
+            // course.students.push(student);
+
+            // await student.save();
+            // await course.save();
+
+            return res.send({ message: `${student.name}  ${student.surname} isimli öğrenci ${course.name} kursunandan başarıyla kaldırıldı!`, success: true });
+
+        } else {
+
+            return res.send({ message: `${student.name}  ${student.surname} adlı öğrenci ${course.name} kursuna kayıtlı değil.`, success: false });
+
+        }
+
+
+    } catch (e) {
+        return res.send({ e: e, m: "error", success: false });
+    }
+});
+
+
+
+
+
+
+// ==================== CREATE COURSE =============================================================================================================================
 
 router.post("/addcourse", async (req, res) => {
 
-console.log("test")
     try {
-        
+
         const newCourse = new Course({
-            name : req.body.name,
-            students : []
+            name: req.body.name,
+            students: []
         });
-        
+
         await newCourse.save();
 
-        return res.send({ course:newCourse });
+        return res.send({ course: newCourse });
     } catch (e) {
         return res.send({ e: e, m: "error" });
     }
 });
-
 
 
 
